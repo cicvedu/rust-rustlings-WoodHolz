706aa232b106efe594f275ecc4d08047ec9da50e
WoodHolz <1535980162@qq.com>
2023-08-18 22:02:45 +0800

    update

--- exercises/drive/drive2.rs
+++ exercises/drive/drive2.rs
@@ -15,6 +15,12 @@ fn raw_pointer_to_box(address: usize) -> Box<Foo> {
     // address is a pointer that points to heap.
     // construct Box from this address, and modify Foo's b field to 
     // the string "hello"
+    unsafe {
+            let data = std::ptr::read(address as *const Foo);
+            let mut boxed_foo = Box::from_raw(Box::into_raw(Box::new(data)) as *mut Foo);
+            boxed_foo.b = Some("hello".to_owned());
+            boxed_foo
+        }
 }
 
 
@@ -25,7 +31,11 @@ mod tests {
 
     #[test]
     fn test_success() {
-        let nanos = Instant::now().duration_since(Instant::now().checked_sub(std::time::Duration::new(0, 1)).unwrap()).as_nanos();
+        let nanos = Instant::now()
+            .duration_since(Instant::now()
+                            .checked_sub(std::time::Duration::new(0, 1))
+                            .unwrap())
+            .as_nanos();
 
         let data = Box::new(Foo{
             a: nanos,
@@ -33,12 +43,11 @@ mod tests {
         });
 
         let ptr_1 = &data.a as *const u128 as usize;
-        let ret = raw_pointer_to_box(Box::into_raw(data) as usize);
+        let ret = raw_pointer_to_box(&data as *const _ as usize);
 
         let ptr_2 = &ret.a as *const u128 as usize;
 
-        assert!(ptr_1 == ptr_2);
-        assert!(ret.b == Some("hello".to_owned()));
-
+        assert_eq!(ptr_1, ptr_2);
+        assert_eq!(ret.b, Some("hello".to_owned()));
     }
 }
